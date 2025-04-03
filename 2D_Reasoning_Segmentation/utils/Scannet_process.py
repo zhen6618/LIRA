059a7e27c8d958e2ebe34bb0d51af163e0b39814
zhen6618 <62835460+zhen6618@users.noreply.github.com>
2025-04-03 18:20:54 +0800

    Add files via upload

---
+++ 2D_Reasoning_Segmentation/utils/Scannet_process.py
@@ -0,0 +1,146 @@
+import pickle
+import json
+import cv2
+import numpy as np
+import shutil
+from PIL import Image
+import os
+import time
+import gzip
+
+def mask_to_polygons(mask):
+    # 找到mask中的轮廓，使用cv2.RETR_CCOMP来处理多层结构（多对象分割）
+    contours, hierarchy = cv2.findContours(mask.astype(np.uint8), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+    
+    polygons_list = []
+
+    for i, contour in enumerate(contours):
+        # 只处理最外层的轮廓或嵌套的分割区域
+        if hierarchy[0][i][3] == -1:
+            # 将轮廓点直接转换为 COCO 格式，即平展成 [x1, y1, x2, y2, ..., xn, yn]
+            points = contour.reshape(-1, 2).tolist()
+            flattened_points = [coord for point in points for coord in point]  # 将点展平成单个列表
+            
+            # 添加到COCO格式的分割列表中
+            polygons_list.append(flattened_points)
+
+    return polygons_list
+
+seg_string = "[SEG]"
+# root_path = "dataset/reason_seg/Scannet_2D_Seg_full/"
+root_path = '/root/paddlejob/workspace/env_run/zhouzhen05/Data_SSD3/scannet_grounding/reason_seg/Scannet_2D_Seg_hard/'
+supplement_instruction = " Please give all objects that are helpful in inferring final targets, as well as the category and color of these objects."
+root_rs_path = '/root/paddlejob/workspace/env_run/zhouzhen05/Data_SSD3/scannet_grounding/'
+
+# 定义类和颜色的排序顺序
+classes = ['cabinet', 'bed', 'chair', 'sofa', 'table', 'bookshelf', 
+        'picture', 'counter', 'desk', 'curtain', 'refrigerator', 
+        'shower curtain', 'toilet', 'sink', 'bathtub']
+
+colors = ['Green', 'Copper', 'Brown', 'Red', 'White', 'Gold', 'Blue', 'Orange', 'Purple', 'Gray', 'Beige', 'Pink', 'Yellow', 'Silver', 'Black']
+
+# 创建类和颜色的排序字典，用于在排序时提供权重
+class_order = {cls: i for i, cls in enumerate(classes)}
+color_order = {color: i for i, color in enumerate(colors)}
+
+instructions_num = 10  # 每个场景的instruction数量  base: 100, hard: 10
+
+# 定义计算mask中心的方法
+def mask_center(mask):
+    indices = np.argwhere(mask)
+    center = indices.mean(axis=0)  # 计算中心点
+    return tuple(center)
+
+mode = 'val'
+read_path = f'utils/scannetv2_{mode}.txt'   
+
+t_start = time.time()
+print(read_path, mode)
+time.sleep(5)
+    
+with open(read_path, 'r') as split_file: 
+    for scene_name_i, scene_name in enumerate(split_file):
+        print("**************************************************************** tqdm_scene: ", scene_name_i, "****************************************************************")
+        scene_name = scene_name.strip()
+        print("Processing scene_name: ", scene_name, "i: ", scene_name_i, "time: ", time.time() - t_start, "s.")
+
+        # if not scene_name in ['scene0000_00']:
+        #     continue
+
+        for instructions_i in range(instructions_num):
+            # if instructions_i != 646:
+            #     continue
+
+            if not os.path.exists(root_rs_path + f'grounding_scene_rs_infos_hard/{scene_name}_{instructions_i}_reasoning_seg_data.json.gz'):
+                print(scene_name, 'not exists')
+                continue        
+
+            with gzip.open(root_rs_path + f'grounding_scene_rs_infos_hard/{scene_name}_{instructions_i}_reasoning_seg_data.json.gz', "rt", encoding="utf-8") as file:
+                data = json.load(file)
+
+            # 转换成LISA格式
+            for _, d in enumerate(data):
+                json_path = str(instructions_i) + '_' + d['img_path'].replace("/", "_")[1:].replace("jpg", "json")
+                if os.path.exists(root_path + f'{mode}/' + json_path):
+                    print(json_path, 'exists')
+                    continue
+
+                j_data = {}
+                
+                candidate_len = len(d['candidate'])
+                if candidate_len == 0:
+                    j_data['outputs'] = "No object."
+                else:
+                    candidate_data = d['candidate']
+
+                    for kk in range(candidate_len):
+                        candidate_data[kk]['mask'] = np.unpackbits(np.array(candidate_data[kk]['mask'], dtype=np.uint8)).astype(bool)
+
+                        if candidate_data[kk]['mask'].shape[0] != 968 * 1296:
+                            candidate_data[kk]['mask'] = candidate_data[kk]['mask'].reshape(480, 640)  # 个别场景的mask是480x640
+                        else:
+                            candidate_data[kk]['mask'] = candidate_data[kk]['mask'].reshape(968, 1296)
+
+                    # 按照 class 的顺序排序，接着对同类中的元素按照 color 的顺序排序
+                    sorted_candidate_data = sorted(
+                        candidate_data,
+                        key=lambda x: (class_order.get(x['class'], float('inf')), color_order.get(x['color'], float('inf')), mask_center(x['mask']))
+                    )
+
+                    j_data['outputs'] = ', '.join(f"{item['color'].lower()} {item['class']} {seg_string}" for item in sorted_candidate_data) + "."
+
+                j_data['text'] = [d['instruction'] + supplement_instruction]
+                j_data['is_sentence'] = True
+                j_data['image'] = d['img_path']
+
+                j_data_shapes = {}
+                j_data_shapes['label'] = 'target'
+                j_data_shapes['labels'] = ['target']
+                j_data_shapes['shape_type'] = "polygon"
+                j_data_shapes['image_name'] = d['img_path']
+
+                if candidate_len == 0:
+                    j_data_shapes['points'] = None
+                else:
+                    points = []
+                    for c in range(candidate_len):
+                        points.append(mask_to_polygons(sorted_candidate_data[c]['mask']))  # 已经排好序
+                    j_data_shapes['points'] = points
+
+                j_data_shapes['flags'] = {}
+
+                j_data['shapes'] = [j_data_shapes]
+
+                try:
+                    with open(root_path + f'{mode}/' + json_path, "w") as json_file:
+                        json.dump(j_data, json_file)  
+                except:
+                    continue
+
+                # image_re = Image.open(d['img_path'])
+                # image_re = image_re.resize((1024, 1024))
+                # image_re.save(root_path + 'train/' + all_info['image'])     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  记得在LISA中把图像resize成1024x1024 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+                print(root_path + f'{mode}/' + json_path, 'done.')
+
+print('Done.')
---
+++ 2D_Reasoning_Segmentation/utils/data_processing.py
@@ -0,0 +1,440 @@
+import glob
+import json
+import os
+import time
+import cv2
+import numpy as np
+import re
+import uuid
+from PIL import Image
+from skimage.transform import resize
+from copy import deepcopy
+
+def get_mask_from_json(json_path, img, original_size):
+    try:
+        with open(json_path, "r") as r:
+            anno = json.loads(r.read())
+    except:
+        with open(json_path, "r", encoding="cp1252") as r:
+            anno = json.loads(r.read())
+
+    inform = anno["shapes"]  
+    comments = anno["text"]
+    is_sentence = anno["is_sentence"]
+    answer = anno["outputs"]
+
+    height, width = img.shape[:2]
+
+    ### sort polies by area
+    # area_list = []
+    # valid_poly_list = []
+    for i in inform: 
+        label_id = i["label"]
+        points = i["points"]
+        if "flag" == label_id.lower():  # meaningless deprecated annotations
+            continue
+
+        if points == None:
+            masks = None
+            continue
+        
+        masks = []
+        for pot in points:  # 有几个pot，就有几个instance
+            mask = np.zeros((original_size[0], original_size[1]), dtype=np.uint8)
+            for polygon in pot:
+                polygon = np.array(polygon).reshape(-1, 2)  # 将平展的点转换为 (x, y) 的形状
+                cv2.fillPoly(mask, [polygon.astype(np.int32)], 1)  # 1 表示前景  在mask上填充多边形区域
+            mask = resize(mask.astype('float'), (1024, 1024), order=1, mode="constant", anti_aliasing=False)  # resize后变成浮点数
+            mask[mask >= 0.5] = 1
+            mask[mask < 0.5] = 0
+            masks.append(mask)
+
+        # # 按照从左上到右下的顺序，给mask排序
+        # if len(masks) > 1:
+        #     # 计算 mask 的中心点
+        #     def get_center(mask):
+        #         coords = np.argwhere(mask == 1)  # 找到 mask 中所有非零点的坐标
+        #         center = coords.mean(axis=0)  # 计算中心点坐标
+        #         return tuple(center)
+
+        #     masks = sorted(masks, key=get_center)  # 根据中心点的 y, x 坐标排序，从左上到右下
+        #     # # 查看排序后的结果
+        #     # for m in masks:
+        #     #     print(get_center(m))  # 输出中心点坐标，检查是否按顺序排列
+
+        for mi in range(len(masks)):
+            masks[mi] = np.expand_dims(masks[mi], axis=0)
+        masks = np.concatenate(masks, axis=0)
+    # import pdb;pdb.set_trace()
+
+    '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!   开放词汇   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+    # open_vocab = True
+    # if open_vocab:
+    #     comments = deepcopy(anno["outputs"])  # 使用正则表达式去除 [SEG] 和其前后的空格
+    #     comments = re.sub(r'\s*\[SEG\]\s*', '', comments)
+    #     comments = [comments]
+
+    return masks, comments, is_sentence, answer
+
+furniture_dict = {
+    'cabinet': '柜子',
+    'bed': '床',
+    'chair': '椅子',
+    'sofa': '沙发',
+    'table': '桌子',
+    'bookshelf': '书架',
+    'picture': '画',
+    'counter': '柜台',
+    'desk': '书桌',
+    'curtain': '窗帘',
+    'refrigerator': '冰箱',
+    'shower curtain': '浴帘',
+    'toilet': '马桶',
+    'sink': '水槽',
+    'bathtub': '浴缸'
+}
+
+# 生成一个唯一的uuid
+def generate_uuid():
+    return str(uuid.uuid4())
+
+# 将mask转为多边形
+def mask_to_polygon(mask):
+    # 将mask转为二值图像（假设mask是灰度图或二值图）
+    _, binary_mask = cv2.threshold(mask * 255, 127, 255, cv2.THRESH_BINARY)
+
+    # 确保binary_mask是uint8类型
+    binary_mask = binary_mask.astype(np.uint8)
+
+    # 查找轮廓
+    contours, _ = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # 如果找到了轮廓
+    if contours:
+        # 选择最长的轮廓，按照周长计算，确保传递了`closed=True`
+        longest_contour = max(contours, key=lambda contour: cv2.arcLength(contour, closed=True))
+        
+        # 将轮廓点转换为多边形形式
+        approx_polygon = cv2.approxPolyDP(longest_contour, epsilon=2, closed=True)
+        return approx_polygon
+
+    return []  # 如果没有轮廓，返回空列表
+
+# 转换为JSON格式
+def mask_to_json(mask, mask_type, text, template):
+    mask = mask.astype(np.uint8)
+    elements = []
+
+    for i in range(mask.shape[0]):
+        polygon = mask_to_polygon(mask[i])
+        
+        polygon_points = [{"x": float(pt[0][0]), "y": float(pt[0][1])} for pt in polygon]
+
+        cur_infos = {
+            "attribute": {"type": mask_type[i]},
+            "uuid": generate_uuid(),
+            "markType": "area",
+            "typeId": "area_1",
+            "text": text[i],
+            "points": polygon_points,
+            "rotation": 0,
+        }
+        elements.append(cur_infos)
+
+    # output = {'result': [{'elements': elements}]}
+
+    return elements
+
+
+if __name__ == "__main__":
+    is_dir = False
+
+    if is_dir:
+        # data_dir = "/root/paddlejob/workspace/env_run/zhouzhen05/Data_SSD3/scannet_grounding/reason_seg/Scannet_2D_Seg_base/val"
+        # vis_dir = "/root/paddlejob/workspace/env_run/zhouzhen05/Data_SSD3/scannet_grounding/reason_seg/Scannet_2D_Seg_base/vis"
+        data_dir = "/home/users/zhouzhen05/LISA/dataset/reason_seg/Scannet_2D_Seg_base/val_all"
+        vis_dir = "/home/users/zhouzhen05/LISA/dataset/reason_seg/Scannet_2D_Seg_base/vis"
+        if not os.path.exists(vis_dir):
+            os.makedirs(vis_dir)
+
+        # 模板
+        with open('2D_答案.json', 'r', encoding='utf-8') as file:
+            template = json.load(file)
+
+        json_path_list = sorted(glob.glob(data_dir + "/*.json"))
+        import random
+        random.seed(42)
+        json_path_list = random.sample(json_path_list, 10000)
+
+        count = 0
+        # mapping_left = []
+        # mapping_right = []
+
+        image_names =[]
+        image_links = []
+        image_attributes = []
+
+        for json_path in json_path_list:
+            with open(json_path, "r") as r:
+                anno_json = json.loads(r.read())
+
+            # img_path = json_path.replace(".json", ".png")
+            img_path = anno_json['image']
+            img_path = img_path.replace("/root/paddlejob/workspace/env_run/zhouzhen05/Data/zhouzhen05/Data", "/home/users/zhouzhen05/Reason_Seg_Data")
+            img = cv2.imread(img_path)[:, :, ::-1]
+            original_size = img.shape[:2]
+            img = cv2.resize(img, (1024, 1024))
+
+            # In generated mask, value 1 denotes valid target region, and value 255 stands for region ignored during evaluaiton.
+            mask, comments, is_sentence, answer = get_mask_from_json(json_path, img, original_size)
+
+            if mask is not None:
+                count += 1
+                if count > 2000:
+                    print("Over images")
+                    break
+                
+                '-----------------------------  用于标注 start  -----------------------------'
+                text_label = [match.strip() for match in re.findall(r'\b(\w+)\s(?=\[SEG\])', anno_json['outputs'])]
+                text_label_CN = [furniture_dict[text_l] for text_l in text_label] 
+
+                elements = mask_to_json(mask, mask_type=text_label, text=text_label_CN, template=template)
+                json_out = deepcopy(template)
+                json_out['result'][0]['elements'] = elements
+                json_out['result'][0]['relations']['inclusion'] = []
+                json_out['result'][0]['size']['width'] = 1024
+                json_out['result'][0]['size']['height'] = 1024
+
+                image_names.append(f'{count}.jpg')
+                image_links.append([])
+                image_attributes.append(json_out)
+
+                '-----------------------------  用于标注 end -----------------------------'
+
+                np.random.seed(0)  # For consistent color selection
+                colors = np.random.randint(0, 255, size=(mask.shape[0], 3))
+                vis_img = img.copy()
+
+                origin_img_path = os.path.join(vis_dir, f'{count}.jpg')
+                image = Image.fromarray(img)
+                image.save(origin_img_path)
+
+                for i in range(mask.shape[0]):
+                    # visualization. Green for target, and red for ignore.
+                    valid_mask = (mask[i] == 1).astype(np.float32)[:, :, None]
+                    ignore_mask = (mask[i] == 255).astype(np.float32)[:, :, None]
+
+                    # Apply colors for each instance based on the mask
+                    color = colors[i]
+                    vis_img = vis_img * (1 - valid_mask) * (1 - ignore_mask) + (
+                        (color * 0.6 + img * 0.4) * valid_mask
+                        + (np.array([255, 0, 0]) * 0.6 + img * 0.4) * ignore_mask
+                    )
+
+                vis_img = np.concatenate([img, vis_img], 1)
+                
+                vis_path = os.path.join(vis_dir, f'anno_{count}.jpg')
+                cv2.imwrite(vis_path, vis_img[:, :, ::-1])
+                print("Visualization has been saved to: ", vis_path)
+
+                # mapping_left.append(f'{count}.jpg') 
+                # mapping_right.append(text_label)
+
+        '-----------------------------  用于标注 start  -----------------------------'
+        with open(os.path.join(vis_dir, 'infos.txt'), "w", encoding="utf-8") as file:
+            for i in range(len(image_names)):
+                # 将属性字典转换为 JSON 格式的字符串（以便更清晰地表示复杂数据）
+                attributes_str = json.dumps(image_attributes[i], ensure_ascii=False)
+                
+                # 写入每一行的数据：图片名、图片链接、属性信息
+                file.write(f"{image_names[i]}   {image_links[i]}   {attributes_str}\n")
+        '-----------------------------  用于标注 end -----------------------------'
+
+        # with open(os.path.join(vis_dir, 'mapping.txt'), 'w', encoding='utf-8') as file:
+        #     for l, r in zip(mapping_left, mapping_right):
+        #         # 生成要写入的内容，使用空格进行分隔
+        #         line_content = f"{l}{' ' * 5}{r}\n"
+        #         file.write(line_content)
+
+    else:
+        base_path = "/root/paddlejob/workspace/env_run/zhouzhen05/Data_SSD3/scannet_grounding/reason_seg/Scannet_2D_Seg_base_new/val"
+        json_path = os.path.join(base_path, "50_root_paddlejob_workspace_env_run_zhouzhen05_Data_zhouzhen05_Data_scans_scene0107_00_color_color_2014.json")
+        vis_path = "chat_label_watch.png"
+
+        with open(json_path, "r") as r:
+            anno_json = json.loads(r.read())
+
+        instruction = anno_json['text']
+        output = anno_json['outputs']
+        print("instruction: ", instruction)
+        print("output: ", output)
+
+        # img_path = json_path.replace(".json", ".png")
+        img_path = anno_json['image']
+        print("img_path: ", img_path)
+        img = cv2.imread(img_path)[:, :, ::-1]
+        original_size = img.shape[:2]
+        img = cv2.resize(img, (1024, 1024))
+
+        # In generated mask, value 1 denotes valid target region, and value 255 stands for region ignored during evaluaiton.
+        mask, comments, is_sentence, answer = get_mask_from_json(json_path, img, original_size)
+        print('comments: ', comments)
+        print('answer: ', answer)
+
+        if mask is not None:
+            np.random.seed(0)  # For consistent color selection
+            colors = np.random.randint(0, 255, size=(mask.shape[0], 3))
+            vis_img = img.copy()
+
+            for i in range(mask.shape[0]):
+                # visualization. Green for target, and red for ignore.
+                valid_mask = (mask[i] == 1).astype(np.float32)[:, :, None]
+                ignore_mask = (mask[i] == 255).astype(np.float32)[:, :, None]
+
+                # Apply colors for each instance based on the mask
+                color = colors[i]
+                vis_img = vis_img * (1 - valid_mask) * (1 - ignore_mask) + (
+                    (color * 0.6 + img * 0.4) * valid_mask
+                    + (np.array([255, 0, 0]) * 0.8 + img * 0.2) * ignore_mask
+                )
+
+            vis_img = np.concatenate([img, vis_img], 1)
+            
+            cv2.imwrite(vis_path, vis_img[:, :, ::-1])
+            print("Visualization has been saved to: ", vis_path)
+
+        else:
+            print("mask is none.")
+
+
+    # /root/paddlejob/workspace/env_run/zhouzhen05/Data_SSD3/scannet_grounding/reason_seg/Scannet_2D_Seg_base/human_annotaion
+
+    '-----------------------------  ScanNetV2的2D Segmentation Annotation  -----------------------------' 
+    # custom_colors = [
+    #     (255, 0, 0),        # 红色
+    #     (0, 255, 0),        # 绿色
+    #     (0, 0, 255),        # 蓝色
+    #     (255, 255, 0),      # 黄色
+    #     (255, 0, 255),      # 品红色
+    #     (0, 255, 255),      # 青色
+    #     (255, 128, 0),      # 橙色
+    #     (128, 0, 255),      # 紫色
+    #     (0, 128, 255),      # 天蓝色
+    #     (255, 255, 128),    # 浅黄色
+    #     (128, 255, 128),    # 浅绿色
+    #     (128, 128, 255),    # 浅蓝色
+    #     (255, 128, 128),    # 浅红色
+    #     (128, 255, 255),    # 浅青色
+    #     (255, 128, 255),    # 浅品红色
+    #     (192, 192, 192),    # 灰色
+    #     (255, 165, 0),      # 橘黄色
+    #     (0, 255, 128),      # 绿色-蓝色
+    #     (128, 255, 255)     # 淡青色
+    # ]
+
+    # image = cv2.imread(img_path)
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # scene_name = img_path.split('/color/')[0].split('/')[-1]
+    # img_id = img_path.split('.jpg')[0].split('_')[-1]
+
+    # scannet_anno_path = f'/root/paddlejob/workspace/env_run/zhouzhen05/Data_SSD3/scannet_2d_annotations/{scene_name}/instance-filt/{img_id}.png'
+    # label_image = Image.open(scannet_anno_path)
+    # label_array = np.array(label_image)
+
+    # unique_label_array = np.unique(label_array)
+    # label_mapping = {val: i + 1 for i, val in enumerate(unique_label_array)}  # unique映射为正整数
+    # label_array = np.vectorize(label_mapping.get)(label_array)
+
+    # color_label = np.zeros((label_array.shape[0], label_array.shape[1], 3), dtype=np.uint8)
+    # for i in range(len(custom_colors)):
+    #     color_label[label_array == i] = custom_colors[i]
+
+    # alpha = 0.6
+    # overlayed = cv2.addWeighted(image, 1 - alpha, color_label, alpha, 0)
+
+    # Image.fromarray(overlayed).save('chat_label_watch_scannet.png')
+    
+
+    # print("Done.")
+
+    '-----------------------------  SAM 点分割 -----------------------------'
+    image = Image.open('vs_scannet_3.png')
+
+    sam_image = image.convert("RGB")
+    sam_image = np.array(sam_image)
+
+    from segment_anything import SamPredictor, sam_model_registry
+    sam = sam_model_registry["vit_h"](checkpoint="/root/paddlejob/workspace/env_run/zhouzhen05/Code/DRecon/LLaMA_Factory/generate_color_label/sam_vit_h_4b8939.pth")
+    sam.to(device="cuda:1")
+    sam_predictor = SamPredictor(sam)
+
+    sam_predictor.set_image(sam_image)
+
+    # sam_input_point = np.array([[500, 375], [1125, 625]])
+    sam_input_point = np.array([[300, 300], [600, 100], [450, 200]])  # (x, y)
+
+    # 逐步添加点并更新掩膜
+    input_point = np.zeros((0, 2)) 
+    input_label = np.zeros((0))
+
+    # 存储初始掩膜
+    sam_masks = None
+    sam_masks_input = None
+
+    for sam_i in range(len(sam_input_point)):
+        # 添加新点
+        input_point = np.concatenate((input_point, [sam_input_point[sam_i]]), axis=0)
+        input_label = np.concatenate((input_label, [1]), axis=0)  # 1表示positive point, 0表示negative point
+
+        if sam_masks is None:  # 如果是第一次添加点，则直接进行推理
+            sam_masks, scores, logits = sam_predictor.predict(
+                point_coords=input_point,        # 输入坐标点
+                point_labels=input_label,         # 输入点标签
+                multimask_output=True             # 获取多个掩膜候选
+            )
+        elif sam_i != len(sam_input_point) - 1:   # 使用之前的掩膜与当前点一起进行推理
+            sam_masks_input = logits[np.argmax(scores), :, :]  # 选择上一次最好的掩膜
+            sam_masks, scores, logits = sam_predictor.predict(
+                point_coords=input_point,         # 输入坐标点
+                point_labels=input_label,         # 输入点标签
+                mask_input=sam_masks_input[None, :, :],# 将上次的掩膜作为提示
+                multimask_output=True            # 获取多个掩膜候选
+            )
+        else:  # 最后一次推理
+            sam_masks_input = logits[np.argmax(scores), :, :]  # 选择上一次最好的掩膜
+            sam_masks, scores, logits = sam_predictor.predict(
+                point_coords=input_point,         # 输入坐标点
+                point_labels=input_label,         # 输入点标签
+                mask_input=sam_masks_input[None, :, :],# 将上次的掩膜作为提示
+                multimask_output=False            # 输出单一掩膜
+            )
+
+    sam_mask = sam_masks[0]
+
+    # 设置透明度 (0.0为完全透明, 1.0为完全不透明)
+    alpha = 0.5
+
+    image_data = np.array(image)
+
+    red_image = np.zeros_like(image)
+    red_image[:, :, 0] = 255
+    masked_red_image = red_image * sam_mask[:, :, None]
+
+    overlayed = cv2.addWeighted(image_data, 1 - alpha, masked_red_image, alpha, 0)
+    overlayed = cv2.cvtColor(overlayed, cv2.COLOR_BGR2RGB)
+
+    # 设置点的颜色和半径
+    point_color = (0, 255, 0)  # 绿色
+    point_radius = 5  # 点的半径
+
+    # # 在overlayed图像上绘制点
+    # for point in sam_input_point:
+    #     # 注意OpenCV的坐标顺序是 (x, y)
+    #     x, y = point
+    #     cv2.circle(overlayed, (x, y), point_radius, point_color, -1)  # -1表示填充圆形
+
+    cv2.imwrite("sam_point.png", overlayed)
+
+    print('Done.')
+
