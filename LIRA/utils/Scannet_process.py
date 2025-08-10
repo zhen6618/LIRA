4de0d8c72a82e8951f90cd1b6fa0b837f39df4bc
zhen6618 <62835460+zhen6618@users.noreply.github.com>
2025-08-10 19:14:38 +0800

    Update Scannet_process.py

--- LIRA/utils/Scannet_process.py
+++ LIRA/utils/Scannet_process.py
@@ -9,19 +9,19 @@ import time
 import gzip
 
 def mask_to_polygons(mask):
-    # 找到mask中的轮廓，使用cv2.RETR_CCOMP来处理多层结构（多对象分割）
+    # Find the contours in the mask and use cv2.RETR_CCOMP to process multi-layer structures (multi-object segmentation)
     contours, hierarchy = cv2.findContours(mask.astype(np.uint8), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
     
     polygons_list = []
 
     for i, contour in enumerate(contours):
-        # 只处理最外层的轮廓或嵌套的分割区域
+        # Only process the outermost contours or nested segmented regions
         if hierarchy[0][i][3] == -1:
-            # 将轮廓点直接转换为 COCO 格式，即平展成 [x1, y1, x2, y2, ..., xn, yn]
+            # Convert the contour points directly to COCO format, that is, flatten them into [x1, y1, x2, y2, ..., xn, yn]
             points = contour.reshape(-1, 2).tolist()
-            flattened_points = [coord for point in points for coord in point]  # 将点展平成单个列表
+            flattened_points = [coord for point in points for coord in point]  
             
-            # 添加到COCO格式的分割列表中
+            # Add to COCO format segmentation list
             polygons_list.append(flattened_points)
 
     return polygons_list
@@ -32,23 +32,22 @@ root_path = '/root/paddlejob/workspace/env_run/zhouzhen05/Data_SSD3/scannet_grou
 supplement_instruction = " Please give all objects that are helpful in inferring final targets, as well as the category and color of these objects."
 root_rs_path = '/root/paddlejob/workspace/env_run/zhouzhen05/Data_SSD3/scannet_grounding/'
 
-# 定义类和颜色的排序顺序
+# Define the sort order for classes and colors
 classes = ['cabinet', 'bed', 'chair', 'sofa', 'table', 'bookshelf', 
         'picture', 'counter', 'desk', 'curtain', 'refrigerator', 
         'shower curtain', 'toilet', 'sink', 'bathtub']
 
 colors = ['Green', 'Copper', 'Brown', 'Red', 'White', 'Gold', 'Blue', 'Orange', 'Purple', 'Gray', 'Beige', 'Pink', 'Yellow', 'Silver', 'Black']
 
-# 创建类和颜色的排序字典，用于在排序时提供权重
+# Create a sorting dictionary of classes and colors to provide weights when sorting
 class_order = {cls: i for i, cls in enumerate(classes)}
 color_order = {color: i for i, color in enumerate(colors)}
 
-instructions_num = 360  # 每个场景的instruction数量
+instructions_num = 360  # The number of instructions per scene
 
-# 定义计算mask中心的方法
 def mask_center(mask):
     indices = np.argwhere(mask)
-    center = indices.mean(axis=0)  # 计算中心点
+    center = indices.mean(axis=0)
     return tuple(center)
 
 mode = 'val'
@@ -78,7 +77,7 @@ with open(read_path, 'r') as split_file:
             with gzip.open(root_rs_path + f'grounding_scene_rs_infos_83_pretrain/{scene_name}_{instructions_i}_reasoning_seg_data.json.gz', "rt", encoding="utf-8") as file:
                 data = json.load(file)
 
-            # 转换成LISA格式
+            # Convert to LISA format
             for _, d in enumerate(data):
                 json_path = str(instructions_i) + '_' + d['img_path'].replace("/", "_")[1:].replace("jpg", "json")
                 if os.path.exists(root_path + f'{mode}/' + json_path):
@@ -97,11 +96,11 @@ with open(read_path, 'r') as split_file:
                         candidate_data[kk]['mask'] = np.unpackbits(np.array(candidate_data[kk]['mask'], dtype=np.uint8)).astype(bool)
 
                         if candidate_data[kk]['mask'].shape[0] != 968 * 1296:
-                            candidate_data[kk]['mask'] = candidate_data[kk]['mask'].reshape(480, 640)  # 个别场景的mask是480x640
+                            candidate_data[kk]['mask'] = candidate_data[kk]['mask'].reshape(480, 640)  # The mask of some scenes is 480x640
                         else:
                             candidate_data[kk]['mask'] = candidate_data[kk]['mask'].reshape(968, 1296)
 
-                    # 按照 class 的顺序排序，接着对同类中的元素按照 color 的顺序排序
+                    # Sort by class, then sort the elements of the same class by color
                     sorted_candidate_data = sorted(
                         candidate_data,
                         key=lambda x: (class_order.get(x['class'], float('inf')), color_order.get(x['color'], float('inf')), mask_center(x['mask']))
@@ -124,7 +123,7 @@ with open(read_path, 'r') as split_file:
                 else:
                     points = []
                     for c in range(candidate_len):
-                        points.append(mask_to_polygons(sorted_candidate_data[c]['mask']))  # 已经排好序
+                        points.append(mask_to_polygons(sorted_candidate_data[c]['mask']))  # Already sorted
                     j_data_shapes['points'] = points
 
                 j_data_shapes['flags'] = {}
@@ -136,7 +135,7 @@ with open(read_path, 'r') as split_file:
 
                 # image_re = Image.open(d['img_path'])
                 # image_re = image_re.resize((1024, 1024))
-                # image_re.save(root_path + 'train/' + all_info['image'])     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  记得在LISA中把图像resize成1024x1024 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                # image_re.save(root_path + 'train/' + all_info['image'])     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  Remember to resize the image to 1024x1024 in LISA !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
                 print(root_path + f'{mode}/' + json_path, 'done.')
 
